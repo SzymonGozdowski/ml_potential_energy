97cab5fc0e7b5fdb0a3510d42c3ea99f549b74ae
SzymonGozdowski <gozdosz@gmail.com>
2025-02-26 14:48:44 +0100

    Add files via upload
    Beginning of a project

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy
+import random
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
